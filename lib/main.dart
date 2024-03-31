886d46659557fd651e710673ce51101b65604437
Hasan Emre Karabacak <hasoka_emre@hotmail.com>
2024-03-31 19:52:02 +0300

    v1

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import "package:flutter/material.dart";
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "To Do App",
+      home: Iskele(),
+    );
+  }
+}
+
+class Iskele extends StatelessWidget {
+  const Iskele({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("To Do App"),
+      ),
+      body: const AnaEkran(),
+    );
+  }
+}
+
+class AnaEkran extends StatefulWidget {
+  const AnaEkran({super.key});
+
+  @override
+  State<AnaEkran> createState() => _AnaEkranState();
+}
+
+class _AnaEkranState extends State<AnaEkran> {
+  TextEditingController t1 = TextEditingController();
+  List alisverisListesi = [];
+
+  elemanEkle() {
+    setState(() {
+      alisverisListesi.add(t1.text);
+      t1.clear();
+    });
+  }
+
+  elemanCikar() {
+    setState(() {
+      alisverisListesi.remove(t1.text);
+      t1.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              itemCount: alisverisListesi.length,
+              itemBuilder: (context, indexNumarasi) => ListTile(
+                subtitle: const Text("Alışveriş Malzemeleri"),
+                title: Text(alisverisListesi[indexNumarasi]),
+              ),
+            ),
+          ),
+          TextField(
+            controller: t1,
+          ),
+          ElevatedButton(
+            onPressed: elemanEkle,
+            child: const Text("Ekle"),
+          ),
+          ElevatedButton(
+            onPressed: elemanCikar,
+            child: const Text("Çıkar"),
+          ),
+        ],
+      ),
+    );
+  }
+}
